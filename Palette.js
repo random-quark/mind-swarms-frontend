e05ccba8d30d94b25cdb86a03a740f76792238c1
Theodoros <t.papatheodorou@gold.ac.uk>
2016-10-06 13:39:09 +0100

    a bit more work

--- Palette.js
+++ Palette.js
@@ -1,4 +1,4 @@
-var paletteScaleFactor = 2
+var paletteScaleFactor = 1
 
 function Palette(_width, _height, colorData) {
     push()
@@ -8,27 +8,50 @@ function Palette(_width, _height, colorData) {
     this.yPeriod = 1.; // how many lins on the Y axis
     this.turbPower = 2.0; // how much turbulence
     this.turbSize = 170; // noise zoom in factor
-    this.marbleVbo = [
-        []
-    ]
-    this.huesVbo = [
-        []
-    ]
+    this.marbleVbo = createImage(this.palWidth, this.palHeight)
+    this.huesVbo = createImage(this.palWidth, this.palHeight)
     this.hueOffset = random(10000)
-
     this.c = color(colorData[0], 100, 100)
     this.hueRange = colorData[1]
     this.minMarbleBrightness = colorData[2]
-
     this.noiseStep = 0.005
-
     this.randomXoffset = random(1000)
     this.randomYoffset = random(1000)
-
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eyecolor;
-
+    this.createMarble()
+    this.createHues()
     pop()
+}
+
+Palette.prototype.createMarble = function() {
+    push()
+    this.marbleVbo.loadPixels()
+    colorMode(HSB, 1) // FIX ME!!! THIS SHOULD NOT BE HERE!!!! MAYBE A PUSHSTYLE INTHIS FUNCTION?????
+    for (var x = 0; x < this.palWidth; x++) {
+        for (var y = 0; y < this.palHeight; y++) {
+            var xyValue = (x + this.randomXoffset) * this.xPeriod / this.palWidth + (y + this.randomYoffset) * this.yPeriod / this.palHeight + this.turbPower * noise(x / this.turbSize, y / this.turbSize)
+            var sineValue = abs(sin(xyValue * 3.14159))
+            var tempColor = color(0, 1 - sineValue, map(sineValue, 0, 1, this.minMarbleBrightness, 1))
+            this.marbleVbo.set(x, y, tempColor)
+        }
+    }
+    // marbleVbo.endDraw();
+    this.marbleVbo.updatePixels();
+    pop();
+}
+
+Palette.prototype.createHues = function() {
+        // push()
+        // this.huesVbo.loadPixels()
+        // colorMode(HSB, 1)
+        // for (var x=0; x<this.palWidth; x++) {
+        //   var hueVar = hue(this.c) + map(noise(this.hueOffset), 0, 1, -this.hueRange, this.hueRange)
+        //   if (hueVar<0) hueVar+=1
+        //   // stroke(hueVar, 1, 1)
+        //   stroke(0.5,1,1)
+        //   line(x, 0, x, this.palHeight)
+        //   this.hueOffset+=this.noiseStep
+        // }
+        // this.huesVbo = this.marbleVbo
+        // this.huesVbo.updatePixels()
+        // pop()
 }
--- testPalette.js
+++ testPalette.js
@@ -1,7 +1,15 @@
 function setup() {
+ createCanvas(800,800)
+ tempColorData =  [19, 0.05, 0.9]; // ORANGE
 
+ pal = new Palette(800,800, tempColorData)
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  background(255,0,0)
+  ellipse(50, 50, 80, 80)
+  image(pal.huesVbo,0,0)
+  // pal.huesVbo.loadPixels();
+  // console.log(pal.huesVbo.pixels[50,50])
+  // console.log(pal.minMarbleBrightness)
 }
