e56767cf1e8af28a2a228a1a49384ba8cb3bf1aa
Tom Chambers <tom.chambers@gmail.com>
2016-10-06 19:34:59 +0100

    agents finished

--- Agent.js
+++ Agent.js
@@ -1,48 +1,46 @@
-function Agent(params) {
-    this.settings = {
-        noiseZ: Math.random() * params.interAgentNoiseZRange,
-        speed: params.speed,
-        alpha: params.alpha,
-        maxAngleSpan: params.maxAngleSpan,
-        agentColor: null
+function Agent(canvasSize, agentDefaults) {
+    var settings = {
+        noiseZ: Math.random() * agentDefaults.interAgentNoiseZRange
     }
+    this.noiseZ = 0
+    this.settings = Object.assign({}, agentDefaults)
     this.setLocation()
-    this.angle = 0
     this.setColor()
 }
 
-Agent.prototype.setColor() = function() {
+Agent.prototype.setLocation = function() {
+    this.location = {
+        current: {
+            x: Math.random() * canvasSize.width, y: Math.random() * canvasSize.height
+        }
+     }
+     this.location.previous = Object.assign({}, this.location.current) // TODO: this may not be supported in older browsers
+}
+
+Agent.prototype.setColor = function() {
     // this.agentColor = palette.getColor(this.location.current)
     this.agentColor = 'black'
 }
 
-Agent.prototype.setLocation() = function() {
-    this.location = {
-        current: {
-            x: Math.random() * width, y: Math.random() * height
-        }
-     }
-    this.location.previous = this.location.current
-}
-
-Agent.prototype.resetAgent() = function() {
+Agent.prototype.resetAgent = function() {
     this.setColor()
     this.setLocation()
 }
 
 Agent.prototype.update = function() {
-    var noiseVal = noise(location.current.x/noiseScale + randomSeed, location.current.y/noiseScale + randomSeed)
-    angle = map(noiseVal, 0, 1, -1, 1)
-    angle = (angle * radians(params.maxAngleSpan) * noiseStrength) + randomInitialDirection
-    location.current.x += cos(angle) * settings.speed
-    location.current.y += sin(angle) * settings.speed
-    if (location.current.x<0 || location.current.x>width || location.current.y<0 || location.current.y>height) this.resetAgent()
+    var noiseVal = noise(this.location.current.x/this.settings.noiseScale + this.settings.randomSeed, this.location.current.y/this.settings.noiseScale + this.settings.randomSeed)
+    var angle = map(noiseVal, 0, 1, -1, 1)
+    angle = (angle * radians(this.settings.maxAngleSpan)) + this.settings.randomInitialDirection
+    this.location.current.x += cos(angle) * this.settings.speed
+    this.location.current.y += sin(angle) * this.settings.speed
+    if (this.location.current.x<0 || this.location.current.x>width || this.location.current.y<0 || this.location.current.y>height) this.resetAgent()
 }
 
 Agent.prototype.draw = function() {
     // make sure alpha, stroke, strokeweight and strokewidth are set correctly
-    // draw a line from previous to current
+    stroke(0, this.settings.agentsAlpha)
+    line(this.location.previous.x, this.location.previous.y, this.location.current.x, this.location.current.y)
 
-    location.previous = Object.create(location.current)
-    noiseZ += noiseStep
+    this.location.previous = Object.assign({}, this.location.current)
+    this.noiseZ += this.settings.noiseStep
 }
--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 window.onload = main
 
+var canvasSize = { width: 500, height: 500 }
+
 var settings = {
-    agents: 1,
-    noiseScale: 150,
-    interAgentNoiseZRange: 0,
-    noiseZStep: 0.001,
+    agents: 5000,
+    fadeAlpha: 5,
     noiseDet: 4,
     overlayAlpha: 0,
+    blendFactor: 0.5,
+    paletteScaleFactor: 2
+}
+
+var agentDefaults = {
     agentsAlpha: 20,
     strokeWidth: 1,
     maxAngleSpan: 220,
-    noiseStrength: 1,
     randomSeed: 0,
     speed: 3,
-    blendFactor: 0.5,
-    paletteScaleFactor: 2
+    noiseScale: 150,
+    interAgentNoiseZRange: 0,
+    noiseZStep: 0.001,
+    randomInitialDirection: 0 // TODO: should this randomized here?
 }
 
 var agents = []
@@ -24,23 +30,28 @@ var data = {
     emotion1: null
 }
 
-function main() {
-    var canvas = document.getElementById('swarm')
-    var ctx = canvas.getContext('2d')
-
+function setup() {
+    console.log('setting up')
+    createCanvas(canvasSize.width, canvasSize.height)
     for (var i=0; i<settings.agents; i++) {
-        var params = {
-
-        }
-        agents.push(new Agent(params))
+        agents.push(new Agent(canvasSize, agentDefaults))
     }
     draw()
 }
 
 function draw() {
+    fill(255, settings.fadeAlpha)
+    noStroke()
+    rect(0, 0, canvasSize.width, canvasSize.height)
     for (var i=0; i<agents.length; i++) {
         agents[i].update()
         agents[i].draw()
     }
-    requestAnimationFrame(draw)
+    // requestAnimationFrame(draw) // only use if initiating draw ourselves
+}
+
+function main() {
+    // var canvas = document.getElementById('swarm') // TODO: do we use our canvas or p5's?
+    // var ctx = canvas.getContext('2d')
+    // draw()
 }
