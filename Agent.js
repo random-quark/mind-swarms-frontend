3561f90ba2b05918e8279f87b4463c89cf4b5797
Tom Chambers <tom.chambers@gmail.com>
2016-10-10 15:18:13 +0100

    wip convert to 3js

--- Agent.js
+++ Agent.js
@@ -1,6 +1,22 @@
-function Agent(canvasSize) {
-    this.settings = Object.assign(settings, agentDefaults)
+function Agent(i, canvasSize) {
+    this.i = i
+    this.settings = Object.assign({}, agentDefaults)
     this.noiseZ = Math.random() * this.settings.interAgentNoiseZRange
+
+    var randLocX1 = Math.random() * 2000 - 1000;
+    var randLocY1 = Math.random() * 2000 - 1000;
+    var randLocX2 = randLocX1 - 5;
+    var randLocY2 = randLocY1 - 5;
+    geometry.vertices.push(
+        new THREE.Vector3(randLocX1, randLocY1, 0),
+        new THREE.Vector3(randLocX2, randLocY2, 0)
+    );
+    this.agentColor = colorMixer.getColor(this.location.current.x, this.location.current.y)
+    this.agentColor[3] = agentDefaults.agentsAlpha    
+    var c = Math.random() * 0xffffff
+    geometry.colors.push(new THREE.Color(c))
+    geometry.colors.push(new THREE.Color(c))
+
     this.setLocation()
     this.setColor()
 }
@@ -8,8 +24,8 @@ function Agent(canvasSize) {
 Agent.prototype.setLocation = function() {
     this.location = {
         current: {
-            x: Math.floor(random(canvasSize.width)),
-            y: Math.floor(random(canvasSize.height))
+            x: Math.floor(Math.random() * canvasSize.width),
+            y: Math.floor(Math.random() * canvasSize.height)
         }
     }
     this.location.previous = {
@@ -19,8 +35,7 @@ Agent.prototype.setLocation = function() {
 }
 
 Agent.prototype.setColor = function() {
-    this.agentColor = colorMixer.getColor(this.location.current.x, this.location.current.y)
-    this.agentColor[3] = agentDefaults.agentsAlpha
+
 }
 
 Agent.prototype.resetAgent = function() {
@@ -35,13 +50,28 @@ Agent.prototype.update = function() {
     angle = (angle * radians(this.settings.maxAngleSpan)) + this.settings.randomInitialDirection
     this.location.current.x += Math.cos(angle) * this.settings.speed
     this.location.current.y += Math.sin(angle) * this.settings.speed
-    if (this.location.current.x < 0 || this.location.current.x > width || this.location.current.y < 0 || this.location.current.y > height) this.resetAgent()
+    if (this.location.current.x < 0 || this.location.current.x > canvasSize.width || this.location.current.y < 0 || this.location.current.y > canvasSize.height) this.resetAgent()
     this.noiseZ += this.settings.noiseZStep
-}
 
-Agent.prototype.draw = function() {
-    stroke(this.agentColor)
-    line(this.location.previous.x, this.location.previous.y, this.location.current.x, this.location.current.y)
+    geometry.vertices[this.i*2].x = this.location.previous.x - (canvasSize.width/2)
+    geometry.vertices[this.i*2].y = this.location.previous.y - (canvasSize.height/2)
+
+    geometry.vertices[this.i*2+1].x = this.location.current.x  - (canvasSize.width/2)
+    geometry.vertices[this.i*2+1].y = this.location.current.y - (canvasSize.height/2)
+
+    // geometry.vertices[this.i*2].x = -1
+    // geometry.vertices[this.i*2].y = -1
+    //
+    // geometry.vertices[this.i*2+1].x = 1
+    // geometry.vertices[this.i*2+1].y = 1
+
+
     this.location.previous.x = this.location.current.x
     this.location.previous.y = this.location.current.y
 }
+
+// Agent.prototype.draw = function() {
+//     // add new position and add to geometry
+//
+//
+// }
--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-  window.onload = main
-
 var canvasSize = {
-    width: 600,
-    height: 400
+    width: 800,
+    height: 800
 }
 
 var settings = {
-    agents: 4000,
+    agents: 60000,
     fadeAlpha: 0,
     noiseDet: 4,
     overlayAlpha: 0,
@@ -40,32 +38,88 @@ var data = {
 
 var colorMixer
 
+//three
+var camera, scene, renderer, geometry
+geometry = new THREE.Geometry();
+
 function setup() {
+    //three
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.set(0, 0, 2000);
+    scene = new THREE.Scene();
+    // var colors = [0xff0080, 0x8000ff, 0xffffff];
+    // var material = new THREE.LineBasicMaterial({
+    //     color: colors[Math.floor(Math.random() * colors.length)],
+    //     opacity: 0.5,
+    //         // linewidth: 5,
+    //     transparent: true
+    // })
+    material = new THREE.MeshBasicMaterial({
+        vertexColors: THREE.VertexColors,
+        opacity: 0.5,
+        transparent: true
+    })
+
+    // colorMixer = new ColorMixer(canvasSize, settings.paletteScaleFactor, settings.customBlend, ["surprise", "anger"], settings.blendFactor)
+
+    for (var i = 0; i < settings.agents; i++) {
+        agents.push(new Agent(i, canvasSize, agentDefaults))
+    }
+    // for (var i = 0; i < settings.agents; i++) {
+    //     var randLocX1 = Math.random() * 2000 - 1000;
+    //     var randLocY1 = Math.random() * 2000 - 1000;
+    //     var randLocX2 = randLocX1 - 5;
+    //     var randLocY2 = randLocY1 - 5;
+    //     geometry.vertices.push(
+    //         new THREE.Vector3(randLocX1, randLocY1, 0),
+    //         new THREE.Vector3(randLocX2, randLocY2, 0)
+    //     );
+    //     var c = Math.random() * 0xffffff
+    //     geometry.colors.push(new THREE.Color(c))
+    //     geometry.colors.push(new THREE.Color(c))
+    // }
+    var line = new THREE.LineSegments(geometry, material);
+    scene.add(line);
+
+    renderer = new THREE.WebGLRenderer({
+        preserveDrawingBuffer: true,
+        antialias: true
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.sortObjects = false;
+    renderer.autoClearColor = false;
+    container.appendChild(renderer.domElement);
+
+    camera.position.x = 0; //+= ( mouseX - camera.position.x ) * .05;
+    camera.position.y = 0; //+= ( - mouseY - camera.position.y ) * .05;
+    camera.lookAt(scene.position);
+
     // TODO: make ajax call to backend to get emotions and amounts
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 
-    colorMixer = new ColorMixer(canvasSize, settings.paletteScaleFactor, settings.customBlend, ["surprise", "anger"], settings.blendFactor)
-    createCanvas(canvasSize.width, canvasSize.height)
-    for (var i = 0; i < settings.agents; i++) {
-        agents.push(new Agent(canvasSize, agentDefaults))
-    }
+
+    // createCanvas(canvasSize.width, canvasSize.height)
 
     document.body.appendChild(stats.dom);
-    strokeWeight(agentDefaults.strokeWidth)
+    // strokeWeight(agentDefaults.strokeWidth)
     myDraw();
 }
 
 function myDraw() {
     stats.begin();
 
-    // fill(255)
-    // noStroke()
-    // rect(0, 0, canvasSize.width, canvasSize.height)
+    // movePoints()
 
     for (var i = 0; i < agents.length; i++) {
         agents[i].update()
-        agents[i].draw()
     }
+    geometry.verticesNeedUpdate = true;
+
+    renderer.render(scene, camera)
 
     if (settings.debug) showVbos()
 
@@ -73,11 +127,11 @@ function myDraw() {
     requestAnimationFrame(myDraw) // only use if initiating draw ourselves
 }
 
-function main() {
-    // var canvas = document.getElementById('swarm') // TODO: do we use our canvas or p5's?
-    // var ctx = canvas.getContext('2d')
-    // draw()
-}
+// function main() {
+//     // var canvas = document.getElementById('swarm') // TODO: do we use our canvas or p5's?
+//     // var ctx = canvas.getContext('2d')
+//     // draw()
+// }
 
 function mousePressed() {
     if (mouseButton === LEFT) settings.imageChoice += 1
